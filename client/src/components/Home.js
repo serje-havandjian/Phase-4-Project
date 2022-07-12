bd22f3442ac988d46387671df61b8472ca0dae23
Serje <Serje.havandjian@gmail.com>
2022-07-12 12:24:31 -0700

    desparate

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ function Home({ user }) {
     if (user) {
       return <h1>Welcome, {user.username}!</h1>;
     } else {
-      return <h1>Please Login or Sign Up</h1>;
+      return <h1>Please Login or Sign Up And Work</h1>;
     }
   }
   
